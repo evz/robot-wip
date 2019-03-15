5319af16d33a7a93752e844f929e82188a5b896f
Eric van Zanten <eric@e-vz.com>
2019-03-15 21:57:00 +0000

    Getting things rotating

--- robot.py
+++ robot.py
@@ -1,8 +1,9 @@
 import time
 import logging
+import math
 
 import brickpi3
-from rubikscubesolver import Side, Square
+# from rubikscubesolver import Side, Square
 
 from base import Motor, Sensor
 
@@ -36,9 +37,41 @@ class Spinner(Motor):
 
 
 class ColorSensor(Sensor):
+
+    #   white = (235, 254, 250)
+    #   green = (20, 105, 74)
+    #   yellow = (210, 208, 2)
+    #   orange = (148, 53, 9)
+    #   blue = (22, 57, 103)
+    #   red = (104, 4, 2)
+
+    NORMALIZED_COLORS = {
+        (235, 254, 250,): 'White',
+        (20, 105, 74,): 'Green',
+        (210, 208, 2,): 'Yellow',
+        (148, 53, 9,): 'Orange',
+        (22, 57, 103,): 'Blue',
+        (104, 4, 2,): 'Red',
+    }
+
     def __init__(self, **kwargs):
         super().__init__(self.PORT_1, **kwargs)
         self.set_sensor_type(self.port, self.SENSOR_TYPE.EV3_COLOR_COLOR_COMPONENTS)
+
+    def get_normalized_color(self):
+        observed_value = self.read_sensor()
+        logger.debug('raw RGB {}, {}, {}'.format(*observed_value[:3]))
+
+        def color_distance(color_tuple_1, color_tuple_2):
+            (red1, green1, blue1,) = color_tuple_1
+            (red2, green2, blue2,) = color_tuple_2
+
+            return math.sqrt(((red1 - red2) ** 2) + ((green1 - green2) ** 2) + ((blue1 - blue2) ** 2))
+
+        comparison_colors = list(self.NORMALIZED_COLORS.keys())
+        sorted_comparisons = sorted(comparison_colors, key=lambda color: color_distance(color, observed_value[:3]))
+        closest_color = sorted_comparisons[0]
+        return self.NORMALIZED_COLORS[closest_color]
 
 
 class Ultrasonic(Sensor):
@@ -84,10 +117,49 @@ class Robot(object):
 
     def read_cube_face(self):
         # self.wait_for_cube()
-        self.color_sensor_arm.set_position(-500)
-        color = self.color_sensor.read_sensor()
+
+        self.color_sensor_arm.set_position(-475)
+        color = self.color_sensor.get_normalized_color()
         logger.info('color {}'.format(color))
 
+        self.color_sensor_arm.set_position_relative(130)
+        color = self.color_sensor.get_normalized_color()
+        logger.info('color {}'.format(color))
+
+        self.spinner.set_position_relative(120)
+        time.sleep(0.2)
+        color = self.color_sensor.get_normalized_color()
+        logger.info('color {}'.format(color))
+
+        self.spinner.set_position_relative(140)
+        time.sleep(0.2)
+        color = self.color_sensor.get_normalized_color()
+        logger.info('color {}'.format(color))
+
+        self.spinner.set_position_relative(140)
+        time.sleep(0.2)
+        color = self.color_sensor.get_normalized_color()
+        logger.info('color {}'.format(color))
+
+        self.spinner.set_position_relative(140)
+        time.sleep(0.2)
+        color = self.color_sensor.get_normalized_color()
+        logger.info('color {}'.format(color))
+
+        self.spinner.set_position_relative(140)
+        time.sleep(0.2)
+        color = self.color_sensor.get_normalized_color()
+        logger.info('color {}'.format(color))
+
+        self.spinner.set_position_relative(140)
+        time.sleep(0.2)
+        color = self.color_sensor.get_normalized_color()
+        logger.info('color {}'.format(color))
+
+        self.spinner.set_position_relative(140)
+        time.sleep(0.2)
+        color = self.color_sensor.get_normalized_color()
+        logger.info('color {}'.format(color))
 
 if __name__ == "__main__":
     import sys
